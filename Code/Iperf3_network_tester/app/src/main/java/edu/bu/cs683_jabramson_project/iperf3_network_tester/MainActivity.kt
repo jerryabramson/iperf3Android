76876d38d71676be965ecf1d4af9c19771e5635d
Jerry Abramson <21062491+jerryabramson@users.noreply.github.com>
2026-03-25 08:10:00 -0400

    archive old activity-based prototype

---
+++ Code/Iperf3_network_tester/app/src/main/java/edu/bu/cs683_jabramson_project/iperf3_network_tester/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.bu.cs683_jabramson_project.iperf3_network_tester
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.bu.cs683_jabramson_project.iperf3_network_tester.ui.theme.Iperf3_network_testerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Iperf3_network_testerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Iperf3_network_testerTheme {
+        Greeting("Android")
+    }
+}
