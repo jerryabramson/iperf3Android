5cfcbf5c1a8d409c7c3145fdb47db6b6155c25d8
Jerry Abramson <21062491+jerryabramson@users.noreply.github.com>
2026-04-13 15:26:52 -0400

    pre deploy script for windows and linux for use with emulator

--- Code/Iperf3_network_tester/app/scripts/pre-deploy.bat
+++ Code/Iperf3_network_tester/app/scripts/pre-deploy.bat
@@ -6,10 +6,10 @@ REM =================================================
 
 >> ..\pre-deploy.log 2>&1
 echo %DATE% %TIME%
+echo  Running pre-deploy script for emulator/device...
 echo "ANDROID_HOME = %ANDROID_HOME%"
 echo "ADB_EXECUTABLE = %ADB_EXECUTABLE%"
 
-echo  Running pre-deploy script for emulator/device...
 echo  List the devices that ADB sees
 %ADB_EXECUTABLE% devices
 echo Turn off SELinux for testing purposes
