ff37c85356a26159beaab1aac35a37c9c76abd35
Jerry Abramson <21062491+jerryabramson@users.noreply.github.com>
2026-04-15 19:08:08 -0400

    fixed pre-deploy script

--- Code/Iperf3_network_tester/app/scripts/pre-deploy.sh
+++ Code/Iperf3_network_tester/app/scripts/pre-deploy.sh
@@ -7,7 +7,7 @@ $ADB_EXECUTABLE devices
 echo "Turn off selinux for testing purposes"
 set -x
 $ADB_EXECUTABLE shell getenforce
-$ADB_EXECUTABLE shell su root  setenforce 0
+$ADB_EXECUTABLE shell -n su -c  setenforce 0
 $ADB_EXECUTABLE shell getenforce
 set +x
 echo "Pre-deploy script completed."
