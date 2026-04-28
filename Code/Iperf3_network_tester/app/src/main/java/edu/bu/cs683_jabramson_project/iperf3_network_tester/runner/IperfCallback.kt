8d39d5ac509dbef801d29068a68e7ca6cf6cb44c
Jerry Abramson <21062491+jerryabramson@users.noreply.github.com>
2026-04-27 23:04:36 -0400

    remove old subdir

--- Code/Iperf3_network_tester/app/src/main/java/edu/bu/cs683_jabramson_project/iperf3_network_tester/runner/IperfCallback.kt
+++
@@ -1,7 +0,0 @@
-package edu.bu.cs683_jabramson_project.iperf3_network_tester.runner
-
-interface IperfCallback {
-    fun onOutput(line: String)
-    fun onError(error: String)
-    fun onComplete()
-}
